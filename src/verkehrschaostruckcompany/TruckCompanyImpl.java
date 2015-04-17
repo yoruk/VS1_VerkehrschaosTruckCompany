00b49dcd168d6181e897a4eafcd575b7397c57d6
Michael Schmidt <michael.schmidt1@haw-hamburg.de>
2015-04-17 12:43:10 +0200

    skelett fuer truckcompanyimpl erzeugt

---
+++ src/verkehrschaostruckcompany/TruckCompanyImpl.java
@@ -0,0 +1,57 @@
+package verkehrschaostruckcompany;
+
+import verkehrschaos.TTruckListHolder;
+import verkehrschaos.Truck;
+import verkehrschaos.TruckCompanyPOA;
+
+public class TruckCompanyImpl extends TruckCompanyPOA {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTruck(Truck truck) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeTruck(Truck truck) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTrucks(TTruckListHolder trucks) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void leave(Truck truck) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void advise(Truck truck) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void arrive(Truck truck) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void putOutOfService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
